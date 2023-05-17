a45056f1312400ce7eba096b2a7e5c0ff6d44dd3
Daniel Goye <danielgoye92@gmail.com>
2023-05-18 00:14:33 +0300

    Create chatbox UI.

--- components/HomePage/widgets/ChatBox.jsx
+++ components/HomePage/widgets/ChatBox.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "@/styles/Home.module.css";
+import SingleChat from "./SingleChat";
 
 const ChatBox = () => {
-  return <div className={styles.chatbox_container}>ChatBox</div>;
+  return (
+    <div className={styles.chatbox_container}>
+      <SingleChat />
+    </div>
+  );
 };
 
 export default ChatBox;
---
+++ components/HomePage/widgets/SingleChat.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setCurrentChat } from "@/redux";
+import styles from "@/styles/Home.module.css";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ProfileModal from "./ProfileModal";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+
+const SingleChat = () => {
+  const dispatch = useDispatch();
+  const selectedChat = useSelector((state) => state.currentChat);
+  const loggedUser = useSelector((state) => state.user);
+  const [openModal, setOpenModal] = useState(false);
+  const [chatSender, setChatSender] = useState(null);
+  useEffect(() => {
+    if (selectedChat) {
+      setChatSender(
+        !selectedChat.isGroupChat
+          ? selectedChat.users.filter((user) => user._id !== loggedUser._id)[0]
+          : null
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedChat]);
+
+  return (
+    <>
+      {selectedChat ? (
+        <div className={styles.select_chat_heading}>
+          <IconButton
+            onClick={() => dispatch(setCurrentChat({ currentChat: null }))}
+          >
+            <ArrowBackIcon />
+          </IconButton>
+          {!selectedChat.isGroupChat ? (
+            <span>{chatSender && chatSender.name}</span>
+          ) : (
+            <span>{selectedChat.chatName}</span>
+          )}
+          {!selectedChat.isGroupChat && (
+            <IconButton onClick={() => setOpenModal(true)}>
+              <VisibilityIcon />
+            </IconButton>
+          )}
+          {chatSender && (
+            <ProfileModal
+              open={openModal}
+              handleClose={() => setOpenModal(false)}
+              image={chatSender.pic}
+              email={chatSender.email}
+            />
+          )}
+        </div>
+      ) : (
+        <div className={styles.select_chat_message}>
+          <span className={styles.select_chat_messageText}>
+            Click on a user to start chatting
+          </span>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SingleChat;
